b06732fbded41ee1b06b8b5b04ed2c6dab744865
v7l-vinda <v7l@vinda>
2024-04-10 15:27:09 +0300

    add, get, remove, clear methods implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        MyArrayList<Integer> sample = new MyArrayList<>(Arrays.asList(10, 20, 30));
-//        sample.add(12);
-        System.out.println(sample.size());
+        MyArrayList<Integer> sample = new MyArrayList<>(Arrays.asList(10, 20, 30, 40));
         System.out.println(sample);
+        sample.add(12);
+        System.out.println(sample);
+        sample.add(3, 3);
+        System.out.println(sample);
+        sample.remove(2);
+        System.out.println(sample);
+        for (int i = 0; i < 10; i++) {
+            sample.add(2);
+        }
+        System.out.println(sample);
+        List<Integer> librarylist = new ArrayList<>(Arrays.asList(10, 20, 30, 40));
+        System.out.println(librarylist);
     }
 }
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -3,6 +3,32 @@ package org.example;
 import java.util.Arrays;
 import java.util.Collection;
 
+/*
+Реализовать свой ArrayList.
+ Методы, обязательные к реализации:
+- добавить элемент: add(T element),
+- добавить элемент: по индексу: add(int index, T element),
+- получить элемент: get(int index),
+- удалить элемент remove(int index),
+- очистить всю коллекцию: clear(),
+- отсортировать sort() и (или) sort(Comparator<T> comparator),
+остальное по желанию.
+
+4. Реализовать алгоритм quicksort для реализованного вами ArrayList.
+Ваш QuickSort должен принимать список любого типа и сортировать его. Использовать:
+○ Java generics
+○ Comparable, Comparator
+
+5. Документировать код. Что, зачем и как. Все классы и интерфейсы должны быть
+задокументированы на уровне класса (class-level javadoc). Все публичные
+методы ваших реализаций должны содержать javadoc. Документация должна
+быть в полном объёме и представлять исчерпывающее и интуитивно понятное
+руководство пользования вашим кодом для другого разработчика.
+
+6. Все части кода должны быть покрыты Unit тестами.
+7. Необходимо убедиться, что тесты запускаются с различными типами данных и большим количеством данных.
+*/
+
 public class MyArrayList<E> {
     private int size = 0;
     private int capacity = 10;
@@ -26,6 +52,70 @@ public class MyArrayList<E> {
 
     public int size() {
         return size;
+    }
+
+    public void add(E element) {
+        if (size == capacity) {
+            realloc();
+        }
+        array[size] = element;
+        size++;
+    }
+
+    public void add(int index, E element) {
+        if (index > capacity) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (size == capacity) {
+            realloc();
+        }
+        if (array[index] != null) {
+            for (int i = size; i >= index; i--) {
+                array[i + 1] = array[i];
+            }
+        }
+        array[index] = element;
+        size++;
+    }
+
+    public E get(int index) {
+        if (index > capacity) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    public void remove(int index) {
+        if (index > capacity) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (index == size) {
+            array[index] = null;
+        } else {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            array[size] = null;
+        }
+        if (size > 0) {
+            size--;
+        }
+    }
+
+    public void clear() {
+        size = 0;
+        capacity = 10;
+        E[] newArray = (E[]) new Object[capacity];
+        array = newArray;
+    }
+
+    private void realloc() {
+        capacity *= 2;
+        E[] newArray = (E[]) new Object[capacity];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
     }
 
     @Override
